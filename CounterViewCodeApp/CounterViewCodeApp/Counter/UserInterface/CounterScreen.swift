7612630188e4c5625832502ba3964fd450f1b9c3
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-02-10 16:44:13 -0300

    feat: receive a CounterScreen and CounterViewModel via dependency injection in the CounterViewController

---
+++ CounterViewCodeApp/CounterViewCodeApp/Counter/UserInterface/CounterScreen.swift
@@ -0,0 +1,12 @@
+//
+//  CounterScreen.swift
+//  CounterViewCodeApp
+//
+//  Created by Gabriel Merenfeld on 10/02/26.
+//
+
+import UIKit
+
+class CounterScreen: UIView {
+    
+}
--- CounterViewCodeApp/CounterViewCodeApp/Counter/UserInterface/CounterViewController.swift
+++ CounterViewCodeApp/CounterViewCodeApp/Counter/UserInterface/CounterViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class CounterViewController: UIViewController {
-
+    
+    private let viewModel: CounterViewModel
+    private let contentView: CounterScreen
+    
+    init(viewModel: CounterViewModel, view: CounterScreen) {
+        self.viewModel = viewModel
+        contentView = view
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = contentView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
 
