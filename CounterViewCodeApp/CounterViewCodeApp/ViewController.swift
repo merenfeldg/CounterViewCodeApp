47ac1fffba769250f0c5b2ae7f2ccacb3363816d
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-02-10 16:01:46 -0300

    feat: create project

---
+++ CounterViewCodeApp/CounterViewCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterViewCodeApp
+//
+//  Created by Gabriel Merenfeld on 10/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
