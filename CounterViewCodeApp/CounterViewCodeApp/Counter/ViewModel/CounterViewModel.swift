f847f3c2013586d516be086751d7afdd3d7f0efd
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-02-10 16:26:09 -0300

    feat: implement CounterViewModel

--- CounterViewCodeApp/CounterViewCodeApp/Counter/ViewModel/CounterViewModel.swift
+++ CounterViewCodeApp/CounterViewCodeApp/Counter/ViewModel/CounterViewModel.swift
@@ -5,6 +5,22 @@
 //  Created by Gabriel Merenfeld on 10/02/26.
 //
 
-class CounterViewModel {
+struct CounterViewModel {
     
+    var onCountValueChange: ((Int) -> Void)?
+    
+    private let minimumValue: Int = 0
+    private var counterModel = CounterModel()
+    
+    mutating func incrementValue() {
+        counterModel.value += 1
+        onCountValueChange?(counterModel.value)
+    }
+    
+    mutating func decrementValue() {
+        if counterModel.value > minimumValue {
+            counterModel.value -= 1
+            onCountValueChange?(counterModel.value)
+        }
+    }
 }
